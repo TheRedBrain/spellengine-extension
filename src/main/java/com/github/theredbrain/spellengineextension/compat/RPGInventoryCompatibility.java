9aeee4ebce746aaa34d6c490bde1998a9b06b11b
TheRedBrain <thetrueredbrain@gmail.com>
2025-09-15 14:45:05 +0200

    fixed RPG Inventory integration

--- src/main/java/com/github/theredbrain/spellengineextension/compat/RPGInventoryCompatibility.java
+++ src/main/java/com/github/theredbrain/spellengineextension/compat/RPGInventoryCompatibility.java
@@ -26,26 +26,26 @@ public class RPGInventoryCompatibility {
 				if (otherItemStack.isEmpty()) {
 					newHasConditionalSpellContainerComponent = new HasConditionalSpellContainerComponent(hasConditionalSpellContainerComponent.is_main_hand_valid(), hasConditionalSpellContainerComponent.is_off_hand_valid(), true, hasConditionalSpellContainerComponent.is_dual_wielding_valid(), hasConditionalSpellContainerComponent.dual_wielding_tag(), true);
 					itemStack.set(SpellEngineExtension.HAS_CONDITIONAL_SPELL_CONTAINER, newHasConditionalSpellContainerComponent);
-					playerInventory.main.set(playerInventory.selectedSlot, itemStack);
+					((DuckPlayerInventoryMixin) playerInventory).rpginventory$setHand(itemStack);
 				} else {
 					newHasConditionalSpellContainerComponent = new HasConditionalSpellContainerComponent(hasConditionalSpellContainerComponent.is_main_hand_valid(), hasConditionalSpellContainerComponent.is_off_hand_valid(), true, hasConditionalSpellContainerComponent.is_dual_wielding_valid(), hasConditionalSpellContainerComponent.dual_wielding_tag(), false);
 					itemStack.set(SpellEngineExtension.HAS_CONDITIONAL_SPELL_CONTAINER, newHasConditionalSpellContainerComponent);
-					playerInventory.main.set(playerInventory.selectedSlot, itemStack);
+					((DuckPlayerInventoryMixin) playerInventory).rpginventory$setHand(itemStack);
 				}
 			} else if (hasConditionalSpellContainerComponent.is_dual_wielding_valid() && !hasConditionalSpellContainerComponent.dual_wielding_tag().isEmpty()) {
 				if (!otherItemStack.isEmpty() && otherItemStack.isIn(TagKey.of(RegistryKeys.ITEM, Identifier.of(hasConditionalSpellContainerComponent.dual_wielding_tag())))) {
 					newHasConditionalSpellContainerComponent = new HasConditionalSpellContainerComponent(hasConditionalSpellContainerComponent.is_main_hand_valid(), hasConditionalSpellContainerComponent.is_off_hand_valid(), false, true, hasConditionalSpellContainerComponent.dual_wielding_tag(), true);
 					itemStack.set(SpellEngineExtension.HAS_CONDITIONAL_SPELL_CONTAINER, newHasConditionalSpellContainerComponent);
-					playerInventory.main.set(playerInventory.selectedSlot, itemStack);
+					((DuckPlayerInventoryMixin) playerInventory).rpginventory$setHand(itemStack);
 				} else {
 					newHasConditionalSpellContainerComponent = new HasConditionalSpellContainerComponent(hasConditionalSpellContainerComponent.is_main_hand_valid(), hasConditionalSpellContainerComponent.is_off_hand_valid(), false, true, hasConditionalSpellContainerComponent.dual_wielding_tag(), false);
 					itemStack.set(SpellEngineExtension.HAS_CONDITIONAL_SPELL_CONTAINER, newHasConditionalSpellContainerComponent);
-					playerInventory.main.set(playerInventory.selectedSlot, itemStack);
+					((DuckPlayerInventoryMixin) playerInventory).rpginventory$setHand(itemStack);
 				}
 			} else if (hasConditionalSpellContainerComponent.is_main_hand_valid() != hasConditionalSpellContainerComponent.is_valid()) {
 				newHasConditionalSpellContainerComponent = new HasConditionalSpellContainerComponent(hasConditionalSpellContainerComponent.is_main_hand_valid(), hasConditionalSpellContainerComponent.is_off_hand_valid(), false, hasConditionalSpellContainerComponent.is_dual_wielding_valid(), hasConditionalSpellContainerComponent.dual_wielding_tag(), hasConditionalSpellContainerComponent.is_main_hand_valid());
 				itemStack.set(SpellEngineExtension.HAS_CONDITIONAL_SPELL_CONTAINER, newHasConditionalSpellContainerComponent);
-				playerInventory.main.set(playerInventory.selectedSlot, itemStack);
+				((DuckPlayerInventoryMixin) playerInventory).rpginventory$setHand(itemStack);
 			}
 		}
 	}
