6729f8834468b3beec1aea686acc552a44c52be9
TheRedBrain <thetrueredbrain@gmail.com>
2026-03-01 23:35:41 +0100

    mixin improvements

--- src/main/java/com/github/theredbrain/spellengineextension/mixin/client/MouseMixin.java
+++ src/main/java/com/github/theredbrain/spellengineextension/mixin/client/MouseMixin.java
@@ -2,6 +2,8 @@ package com.github.theredbrain.spellengineextension.mixin.client;
 
 import com.github.theredbrain.spellengineextension.SpellEngineExtension;
 import com.github.theredbrain.spellengineextension.entity.player.DuckPlayerEntityMixin;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.Mouse;
 import net.minecraft.client.network.ClientPlayerEntity;
@@ -10,8 +12,6 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Mouse.class)
 public abstract class MouseMixin {
@@ -20,16 +20,17 @@ public abstract class MouseMixin {
 	@Final
 	private MinecraftClient client;
 
-	@Inject(method = "updateMouse", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;changeLookDirection(DD)V", ordinal = 0), cancellable = true)
-	private void bettercombatextension$updateMouse(CallbackInfo ci) {
+	@WrapOperation(method = "updateMouse", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;changeLookDirection(DD)V", ordinal = 0))
+	private void spellengineextension$updateMouse(ClientPlayerEntity instance, double cursorDeltaX, double cursorDeltaY, Operation<Void> original) {
 		if (SpellEngineExtension.SERVER_CONFIG.enable_movement_locking_spell_casting.get()) {
 			ClientPlayerEntity player = client.player;
 			if (player != null) {
 				var process = ((SpellCasterClient) player).getSpellCastProcess();
 				if ((((DuckPlayerEntityMixin) player).spellengineextension$getMovementLockingTicks() > 0) || (process != null && process.spell().value().active.cast != null && process.spell().isIn(SpellEngineExtension.ENABLES_MOVEMENT_LOCKING_DURING_CASTING))) {
-					ci.cancel();
+					return;
 				}
 			}
 		}
+		original.call(instance, cursorDeltaX, cursorDeltaY);
 	}
 }
